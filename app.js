a89232bb64fb63e270581a8fe31c159c3fd4c2ae
Loctarogar <igrsl1990@gmail.com>
2020-09-29 20:38:20 +0200

    anonimus function 2

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const startGameBtn = document.getElementById('start-game');
 
-const startGame = function() {
+startGameBtn.addEventListener('click', function() {
   console.log('Game is starting...');
-};
-
-startGameBtn.addEventListener('click', startGame);
+});
