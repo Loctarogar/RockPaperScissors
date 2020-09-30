956b6002e163d6bdd0c48d5216c1fec6c96258a0
Loctarogar <igrsl1990@gmail.com>
2020-09-30 19:46:04 +0200

    added player win/lose

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -20,7 +23,34 @@ const getPlayerChoice = function() {
     return DEFAULT_USER_CHOICE;
   }
   return selection;
-}
+};
+
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if(randomValue < 0.34) {
+    console.log(ROCK);
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    console.log(PAPER);
+    return PAPER;
+  } else {
+    console.log(SCISSORS);
+    return SCISSORS;
+  }
+};
+
+const getWinner = function(cChoice, pChoice) {
+  if(cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    cChoice === ROCK && pChoice === PAPER ||
+    cChoice === PAPER && pChoice === SCISSORS||
+    cChoice === SCISSORS && pChoice === ROCK
+  ) {
+    return RESULT_PLAYER_WINS;
+  }
+  return RESULT_COMPUTER_WINS;
+};
 
 startGameBtn.addEventListener('click', function() {
   if (gameIsRunning) {
@@ -30,4 +60,8 @@ startGameBtn.addEventListener('click', function() {
   console.log('Game is starting...');
   const playerChoice = getPlayerChoice();
   console.log(playerChoice);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
+  gameIsRunning = false;
 });
