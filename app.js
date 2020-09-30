4cd53097ec2c7fb48aa93b3198e979aa5dc17d27
Loctarogar <igrsl1990@gmail.com>
2020-09-30 19:31:51 +0200

    player can choose Rock, Paper or Scissors

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const startGameBtn = document.getElementById('start-game');
 
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function() {
+  console.log('in f');
+  const selection = prompt('sldfjl').toUpperCase();
+  if (
+    selection != ROCK &&
+    selection != PAPER &&
+    selection != SCISSORS
+  ) {
+    console.log(selection);
+    alert("Invalid choice");
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+}
+
 startGameBtn.addEventListener('click', function() {
+  if (gameIsRunning) {
+    return ;
+  }
+  gameIsRunning = true;
   console.log('Game is starting...');
+  const playerChoice = getPlayerChoice();
+  console.log(playerChoice);
 });
